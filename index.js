ee5d18006830b3122b130fdc79ad8abb333efeeb
Joserraul <joserraul07@gmail.com>
2024-11-29 14:33:13 -0400

    actualizacion

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const url = 'https://pokedex2.p.rapidapi.com/pokedex/uk';
+/**const url = 'https://pokedex2.p.rapidapi.com/pokedex/uk';
 const options = {
 	method: 'GET',
 	headers: {
@@ -18,4 +18,25 @@ try {
 } catch (error) {
   console.error(error);
 }
+*/
 
+const url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'fb08216d1bmsh2ed7f5941266469p1052cdjsnb666b30b9832',
+		'x-rapidapi-host': 'pokedex2.p.rapidapi.com'
+	}
+};
+
+async function getPokemon() {
+  const response = await fetch(url, options);
+  const result = await response.json();
+  console.log(result);
+}
+
+try {
+  getPokemon();
+} catch (error) {
+  console.error(error);
+}
